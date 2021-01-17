c16343e07b260b217e3ba7720605b36f6d2aadfa
amadou-20 <ahmo300@outlook.com>
2021-01-17 19:00:33 +0100

    add

--- src/Post.js
+++ src/Post.js
@@ -5,8 +5,7 @@ function Post() {
 
     return (
         <div className="post">
-            <h1>Username</h1>
-
+            <h1>Username and password</h1>
             <img 
             src="https://cdn.pixabay.com/photo/2016/08/09/17/52/instagram-1581266_960_720.jpg"
             alt="abc"
