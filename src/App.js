2f2bf4767f8e4d7132516257ce0b3e49d322ee1b
amadou-20 <ahmo300@outlook.com>
2021-01-17 18:51:39 +0100

    Good explanation of your file changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
            <img 
              className="app_headerImage"
              alt="abc" 
-             src="https://e7.pngegg.com/pngimages/712/1009/png-clipart-letter-instagram-font-instagram-text-logo.png"
+             src="https://ih1.redbubble.net/image.366666424.5206/st,small,507x507-pad,600x600,f8f8f8.u3.jpg"
             />
         </div>
 
--- src/Post.js
+++ src/Post.js
@@ -8,7 +8,7 @@ function Post() {
             <h1>Username</h1>
 
             <img 
-            src="https://pixabay.com/fr/illustrations/instagram-symbole-logo-photo-1581266/"
+            src="https://cdn.pixabay.com/photo/2016/08/09/17/52/instagram-1581266_960_720.jpg"
             alt="abc"
             />
             
