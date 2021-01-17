2b4d148963e4f65934cef9810c6b13ecd05deacc
amadou-20 <ahmo300@outlook.com>
2021-01-17 19:22:51 +0100

    Now we add the post.css file here

--- src/Post.js
+++ src/Post.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import "./Post.css";
 
 
 function Post() {
 
     return (
         <div className="post">
-            <h1>Username and password</h1>
+
+            <h1>Username</h1>
             <img 
+            className="post_image"
             src="https://cdn.pixabay.com/photo/2016/08/09/17/52/instagram-1581266_960_720.jpg"
             alt="abc"
             />
-            
+            <h4>Username: caption</h4>
+
         </div>
-    )
+    );
 }
 
 export default Post;
