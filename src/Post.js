29099a7a29363a79d31919ff9d89f51ba4c5657d
amadou-20 <ahmo300@outlook.com>
2021-01-18 20:20:24 +0100

    changes

--- src/Post.js
+++ src/Post.js
@@ -8,19 +8,19 @@ function Post() {
     return (
         <div className="post">
            <div className="post_header">  
-            <Avatar 
+          <Avatar 
            className="post_avatar"
            alt="john"
            src="https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_960_720.png"
            />
             <h1>Username</h1>
+            </div>
             <img 
             className="post_image"
             src="https://cdn.pixabay.com/photo/2016/02/19/17/43/follow-1210793_960_720.png"
             alt="abc"
             />
-            <h4 className="post_text">Username: caption</h4>
-           </div>         
+            <h4 className="post_text">Username: caption</h4>         
         </div>
     );
 }
