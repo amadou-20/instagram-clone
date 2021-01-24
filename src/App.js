e60d3bbf94a57df104114b7d79343f7f2c28751b
amadou-20 <ahmo300@outlook.com>
2021-01-24 16:40:55 +0100

    let's loop through the posts variable withe map

--- src/App.js
+++ src/App.js
@@ -28,12 +28,16 @@ function App() {
             />
         </div>
         <h1>let's build a instagram clone here clever programmers</h1>
-        <a href=""><p>instagram clone here clever programmers</p></a>
-        <li>instagram clone here clever programmers</li>
+
+        {
+          posts.map(post =>  (
+           <Post username={post.username} caption={post.caption imageUrl={post.imageUrl} />
+          ))
+        }
+        
 
         <Post username="Qazi" caption="WOW it works" imageUrl="https://blog.desafiolatam.com/wp-content/uploads/2019/04/react-galaxia.png" />
         <Post username="Jonathan" caption="Hello world" imageUrl="https://cdn.pixabay.com/photo/2020/10/09/19/39/utah-5641320_960_720.jpg" />
-        <Post username="Enrique" caption="it's a new project" imageUrl="https://cdn.pixabay.com/photo/2017/10/07/16/22/river-2826927_960_720.jpg" />
     </div>
   );
 }
