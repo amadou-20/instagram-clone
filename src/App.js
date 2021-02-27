d2638a08dcb93eed7d31e98204ad0cb7cf88134b
amadou-20 <ahmo300@outlook.com>
2021-02-27 21:55:00 +0100

    Adding the modal's style

--- src/App.js
+++ src/App.js
@@ -6,7 +6,31 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Modal } from '@material-ui/core';
 
 
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+
 function App() {
+
 
 
      const [open, setopen] = useState(false);
@@ -32,7 +56,7 @@ function App() {
        setPosts(snapshot.docs.map(doc => ({
          
         id: doc.id,
-        post: doc.data()
+      post: doc.data()
       })));
      })
      }, [input])
